d4b07511ee04efcbff6866b496925eec6d8b0ce6
Fiona <workartsources@gmail.com>
2023-11-16 02:13:15 +0800

    added first api

--- src/server.js
+++ src/server.js
@@ -6,7 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (res) => {
+  res.send("Server is running!");
+});
+
 const port = 3030;
 app.listen(port, () => {
-  console.log("Server is running on port: " + port);
+  console.log("Server is running on port: http://localhost:" + port);
 });
