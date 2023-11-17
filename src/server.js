833b5784666184d7871613b9212b4c97f76c11c7
Fiona <workartsources@gmail.com>
2023-11-17 22:48:30 +0800

    updated

--- src/server.js
+++ src/server.js
@@ -10,6 +10,23 @@ app.get("/", (res) => {
   res.send("Server is running!");
 });
 
+app.post("/api/register", (req, res) => {
+  const { username, password } = req.body;
+  const createdAt = new Date().toISOString().slice(0, 19).replace("T", " ");
+  connection.query(
+    `INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`,
+    [username, password, createdAt],
+    (error, result) => {
+      if (error) {
+        res.send(error);
+        res.status(500).send("An error ocurred while createing the user.");
+      } else {
+        res.send("User created successfully!");
+      }
+    }
+  );
+});
+
 const port = 3030;
 app.listen(port, () => {
   console.log("Server is running on port: http://localhost:" + port);
