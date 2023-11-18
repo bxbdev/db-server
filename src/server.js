4d9b20f64623e57c7c3132f973b322089b21a138
Fiona <workartsources@gmail.com>
2023-11-19 04:02:35 +0800

    feat: added create users table

--- src/server.js
+++ src/server.js
@@ -13,18 +13,41 @@ app.get("/", (req, res) => {
 app.post("/api/register", (req, res) => {
   const { username, password } = req.body;
   const createdAt = new Date().toISOString().slice(0, 19).replace("T", " ");
-  connection.query(
-    `INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`,
-    [username, password, createdAt],
-    (error, result) => {
-      if (error) {
-        res.send(error);
-        res.status(500).send("An error ocurred while createing the user.");
-      } else {
-        res.send("User created successfully!");
-      }
+
+  connection.query("SHOW TABLES LIKE 'users'", (err, tables) => {
+    if (err) {
+      return res
+        .status(500)
+        .send("An error ocurred while checking if table exists.");
     }
-  );
+
+    if (tables.length === 0) {
+      connection.query(`
+        CREATE TABLE users (
+          id INT NOT NULL AUTO_INCREMENT,
+          username VARCHAR(2500) NOT NULL,
+          password VARCHAR(2500) NOT NULL,
+          deleted TINYINT(1) DEFAULT 0,
+          created_at DATETIME NOT NULL,
+          updated_at DATETIME NULL,
+          PRIMARY KEY (id)
+        )
+      `);
+    }
+
+    connection.query(
+      `INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`,
+      [username, password, createdAt],
+      (error, result) => {
+        if (error) {
+          res.send(error);
+          res.status(500).send("An error ocurred while createing the user.");
+        } else {
+          res.send("User created successfully!");
+        }
+      }
+    );
+  });
 });
 
 const port = 3030;
